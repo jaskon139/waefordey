3b125ca85d5983e5ac954f62c10764de87fff5e9
jaskon139 <jaskon139@gmail.com>
2016-08-15 21:24:40 +0800

    Update runtry.sh

--- runtry.sh
+++ runtry.sh
@@ -3,39 +3,39 @@ git clone git@github.com:jaskon139/waefordey2.git
 git clone git@github.com:jaskon139/waefordey5.git
 git clone git@github.com:jaskon139/waefordey6.git
 
-mv /c/msftpserver/waefordey4/sss_b* /c/msftpserver/waefordey
-cd /c/msftpserver/waefordey
+mv /c/msftpsrvr/waefordey4/sss_b* /c/msftpsrvr/waefordey
+cd /c/msftpsrvr/waefordey
 git add sss_b*
 git commit -m "hee42"
 git push
 
 
-mv /c/msftpserver/waefordey4/sss_c* /c/msftpserver/waefordey
-cd /c/msftpserver/waefordey
+mv /c/msftpsrvr/waefordey4/sss_c* /c/msftpsrvr/waefordey
+cd /c/msftpsrvr/waefordey
 git add sss_c*
 git commit -m "hee4sf"
 git push
 
-mv /c/msftpserver/waefordey4/sss_d* /c/msftpserver/waefordey2
-cd /c/msftpserver/waefordey2
+mv /c/msftpsrvr/waefordey4/sss_d* /c/msftpsrvr/waefordey2
+cd /c/msftpsrvr/waefordey2
 git add sss_d*
 git commit -m "hee4s"
 git push
 
-mv /c/msftpserver/waefordey4/sss_e* /c/msftpserver/waefordey2
-cd /c/msftpserver/waefordey2
+mv /c/msftpsrvr/waefordey4/sss_e* /c/msftpsrvr/waefordey2
+cd /c/msftpsrvr/waefordey2
 git add sss_e*
 git commit -m "hee4sdf"
 git push
 
-mv /c/msftpserver/waefordey4/sss_f* /c/msftpserver/waefordey5
-cd /c/msftpserver/waefordey5
+mv /c/msftpsrvr/waefordey4/sss_f* /c/msftpsrvr/waefordey5
+cd /c/msftpsrvr/waefordey5
 git add sss_f*
 git commit -m "hee4rw3e"
 git push
 
-mv /c/msftpserver/waefordey4/sss_g* /c/msftpserver/waefordey5
-cd /c/msftpserver/waefordey5
+mv /c/msftpsrvr/waefordey4/sss_g* /c/msftpsrvr/waefordey5
+cd /c/msftpsrvr/waefordey5
 git add sss_g*
 git commit -m "hee4rssw3e"
 git push
